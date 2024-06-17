ea49ec7c52fe56d24259ca59433f02c0e9f45d7a
JaviJota <165896246+JaviJota@users.noreply.github.com>
2024-06-17 18:10:37 +0000

    Fix functions

--- src/app.js
+++ src/app.js
@@ -1,65 +1,78 @@
 /* eslint-disable */
-// import "bootstrap";
+import "bootstrap";
 import "./style.css";
 
 // import "./assets/img/rigo-baby.jpg";
 // import "./assets/img/4geeks.ico";
-
 // window.onload = function() {
-let simbolTop = document.querySelector(".simbol-top");
-let simbolBottom = document.querySelector(".simbol-bottom");
-let number = document.querySelector(".number");
-let newCardBtn = document.querySelector(".newCard-btn");
-let intervalId;
-
-const simbol = ["♦", "♥", "♠", "♣"];
-
 //Generador de nueva carta aleatoria
-function getRandomCard() {
-  getRandomNumber();
-  getRandomSimbol();
+function updateCard() {
+  const simbolTop = document.querySelector(".simbol-top");
+  const simbolBottom = document.querySelector(".simbol-bottom");
+  const number = document.querySelector(".number");
+  const docSimbol = getRandomSimbol();
+
+  simbolTop.innerHTML = docSimbol;
+  simbolBottom.innerHTML = docSimbol;
+  number.innerHTML = getRandomNumber();
+  getStyleColor(getColor(docSimbol));
 }
 
+const newCardBtn = document.querySelector(".newCard-btn");
 newCardBtn.addEventListener("click", function() {
-  getRandomCard();
+  updateCard();
   startInterval();
 });
 
 //Generar número aleatorio de carta
 function getRandomNumber() {
-  let randomNumber = Math.floor(Math.random() * 12 + 1);
-  if (randomNumber == 1) number.innerHTML = 'As';
-  else if (randomNumber == 10) number.innerHTML = 'J';
-  else if (randomNumber == 11) number.innerHTML = 'Q';
-  else if (randomNumber == 12) number.innerHTML = 'K';
-  else number.innerHTML = randomNumber;
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let randomNumber = Math.floor(Math.random() * 12);
+  console.log("getRandomNumber" + numbers[randomNumber]);
+
+  return numbers[randomNumber];
 }
 
 //Generar palo aleatorio de carta
 function getRandomSimbol() {
-  let randomSimbolNumber = Math.floor(Math.random() * simbol.length);
-  simbolTop.innerHTML = simbol[randomSimbolNumber];
-  simbolBottom.innerHTML = simbol[randomSimbolNumber];
-  if (randomSimbolNumber == 0 || randomSimbolNumber == 1) {
-    simbolTop.style.color = 'red';
-    simbolBottom.style.color = 'red';
-  } else {
-    simbolTop.style.color = 'black';
-    simbolBottom.style.color = 'black';
-  }
+  const simbol = ["♦", "♥", "♠", "♣"];
+  const randomSimbolNumber = Math.floor(Math.random() * simbol.length);
+  console.log("getRandomSimbol" + simbol[randomSimbolNumber]);
+
+  return simbol[randomSimbolNumber];
 }
 
+function getColor(simbol) {
+  let color = "";
+  if (simbol === "♥" || simbol === "♦") {
+    color = "red";
+  } else if (simbol === "♠" || simbol === "♣") {
+    color = "black";
+  }
+  console.log("getColor" + color);
+  return color;
+}
+
+function getStyleColor(color) {
+  const simbolTop = document.querySelector(".simbol-top");
+  const simbolBottom = document.querySelector(".simbol-bottom");
+
+  simbolTop.style.color = color;
+  simbolBottom.style.color = color;
+}
+
+let intervalId;
 //Cambiar de carta cada 10secs
 function startInterval() {
   //Reset del intervalo iniciado al cargar la página
   clearInterval(intervalId);
   //Iniciar el intervalo de nuevo
-  intervalId = setInterval(getRandomCard, 10000);
+  intervalId = setInterval(updateCard, 10000);
 }
 
-//Al cargar la página: generar nueva carta e iniciar intervalo
+// Al cargar la página: generar nueva carta e iniciar intervalo
 window.onload = function() {
-  getRandomCard();
+  updateCard();
   startInterval();
 };
-// };
+// }
